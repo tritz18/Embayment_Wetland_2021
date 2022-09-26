91c6a29a9524bc10b490f5f9c4d72c562bd419c3
tritz18 <61156565+tritz18@users.noreply.github.com>
2022-09-26 13:58:36 -0400

    Added Catch data cleaning

--- Embayment_Wetland_2021.R
+++ Embayment_Wetland_2021.R
@@ -11,6 +11,9 @@ library(tidyverse)
 
 ##################
 
+################################################################################################################################
+#############################           Abiotic Data                                           ################################
+###############################################################################################################################
 
 #### Set working directory, where logger files are ###
 setwd("/Users/thorn/OneDrive/Desktop/Embayment_Wetland_2021/Embayment_Wetland_R_Project/")
@@ -44,9 +47,41 @@ Abiotic_Sum<- Emb_Wetlands_Raw %>%
   summarise(across(c(DO,TEMP), list(mean = mean, max = max, min=min, sd=sd), .names = "{col}_{fn}"))
 
 
-#### Combine Datafiles #### 
+#### Combine Datafiles for Abiotic Final#### 
 Abiotic_Final<- left_join(Abiotic_Hrs,Abiotic_Sum, by=c("Date_Time", "Location")) %>% 
   group_by(Date_Time) %>% 
   mutate(Study_Day = cur_group_id())
+
+###############################################################################################################################
+###################                                   Catch Data                                                ###############
+###############################################################################################################################
+
+#### set working directory to catch data #####
+
+setwd("/Users/thorn/OneDrive/Desktop/Embayment_Wetland_2021/Embayment_Wetland_R_Project/Catch_Data")
+
+#### Format catch data ####
+Catch_Raw<- read.csv("EM_2021_Catch.csv")
+Catch_Raw$Date_Time<-as.POSIXct(Catch_Raw$Date_Time, format="%Y-%m-%d")
+Catch_Raw$Catch<- as.numeric(Catch_Raw$Catch) 
+Catch_Raw$Depth<- as.numeric(Catch_Raw$Depth) 
+
+#### Create depth data set ####
+Depth<- Catch_Raw %>% 
+  group_by(across(c(Date_Time,Location))) %>% 
+  select(Depth) %>%
+  summarise(Mean_Depth=mean(Depth), Max_Depth=max(Depth), Min_Depth=min(Depth))
+
+
+#### Create Catch dataset grouped by species, date and location ####
+Catch_Sum <-Catch_Raw %>%
+  filter(Species %in% c("LMB", "SMB", "LEPOMIS", "CYPRINIDAE", "KILLI", "BBH"), LIFE_STAGE %in% c("YOY")) %>% 
+  group_by(across(c(Location, Species, Date_Time))) %>% 
+  summarise(Catch= (sum(Catch)))
+
+
+#### Join depth to catch data ####
+Catch_Sum <- left_join(Catch_Sum,Depth, by=c("Date_Time", "Location"))
+
 
 
