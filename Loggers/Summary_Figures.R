fa6b5e689eb256b3babb866e47a4b767bdf53e2c
tritz18 <61156565+tritz18@users.noreply.github.com>
2022-10-04 11:10:05 -0400

    Hypoxic Hours Graph

--- Loggers/Summary_Figures.R
+++ Loggers/Summary_Figures.R
@@ -31,19 +31,19 @@ Emb_Wetlands_Raw <- purrr::map_df(f, function(x) {
 
 #### Abiotic hours data creation (1 measurement per day, sites grouped by location) #### 
 Abiotic_Hrs<- Emb_Wetlands_Raw %>% 
-  group_by(across(c(Location, Date_Time))) %>% 
+  group_by(across(c(Location, Date_Time, Site))) %>% 
   select(DO, TEMP) %>%
-  summarise(Hyp_Hrs=sum(DO<3.01)/2, Nor_Hrs=sum(DO>3.00)/2,
-            Temp_Hrs=sum(TEMP>28)/2) 
+  summarise(Hyp_Hrs=sum(DO<=3.99)/2, Nor_Hrs=sum(DO>=4.00)/2,
+            Temp_Hrs=sum(TEMP>=28)/2) 
 
 #### Summary of abiotic conditions (48 measurements --> 1 per day measurements (mean DO , max temp),sites grouped by location ) ####
 Abiotic_Sum<- Emb_Wetlands_Raw %>% 
-  group_by(across(c(Location, Date_Time))) %>% 
+  group_by(across(c(Location, Date_Time, Site))) %>% 
   select(DO,TEMP) %>% 
   summarise(across(c(DO,TEMP), list(mean = mean, max = max, min=min, sd=sd), .names = "{col}_{fn}"))
 
 #### Combine Datafiles for Abiotic Final (1 per day from summarized loggers and abiotic hours file #### 
-Abiotic_Final<- left_join(Abiotic_Hrs,Abiotic_Sum, by=c("Date_Time", "Location")) %>% 
+Abiotic_Final<- left_join(Abiotic_Hrs,Abiotic_Sum, by=c("Date_Time", "Location", "Site")) %>% 
   group_by(Date_Time) %>% 
   mutate(Study_Day = cur_group_id()) ## Add study day instead of date ##
 
@@ -80,8 +80,23 @@ ggplot(EW_Gather, aes(Location, Measurement, group=interaction(Location, Site), 
 ggsave("Embayment_Wetland_Box_Whisker.png", width = 5, height = 4, dpi=300)
 
 ##############################################################################
-####                  Summary - Tables                                   ####
+####                  Abiotic Visualization                          ####
 #############################################################################
 
+EWH_Gather<- Abiotic_Hrs %>% 
+  gather(key = "Abiotic", value = "Hours", -Date_Time, -Location, -Site) %>% 
+  mutate(Abiotic=factor(Abiotic)) 
 
-9
+
+#### Plot with faceting edits to show both y axis labels and unique scales ####
+ggplot(EWH_Gather, aes(Location, Hours, group=interaction(Location, Site), color=Site))+
+  stat_boxplot(geom ='errorbar')+geom_boxplot(fill="#CCCCCC")+
+  scale_color_manual(values = c("#0072B2","#009E73"))+scale_y_continuous(breaks = seq(0,12,1))+
+  theme_bw()+
+  theme(legend.position = "bottom", axis.title.x = element_blank(),
+        strip.text = element_text(size = 12), axis.text = element_text(size=11))+
+  facet_wrap(~Abiotic,labeller = as_labeller(
+    c(Hyp_Hrs = "Hypoxia (DO<3.99 mg/l)", Nor_Hrs = "Normoxia (DO>4.00 mg/l)", Temp_Hrs="Water Temperature > 27°C")))
+ggsave("Embayment_Wetland_Hours_Box_Whisker.png", width = 8, height = 5, dpi=300)
+
+
