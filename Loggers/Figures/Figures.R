1d8637d04be87c3c7177993385ee75ca38de6255
tritz18 <61156565+tritz18@users.noreply.github.com>
2022-10-23 15:09:59 -0400

    Added hourly plot plus removed 0 mg/l line too much

--- Loggers/Figures/Figures.R
+++ Loggers/Figures/Figures.R
@@ -30,6 +30,8 @@ Emb_Wetlands_Raw$Site<- factor(Emb_Wetlands_Raw$Site, levels=c("Littoral", "Exte
 Emb_Wetlands_Raw$Location<- as.factor(Emb_Wetlands_Raw$Location)
 
 ##########################################
+######     Grouping by day ##############
+##########################################
 
 
 Abiotic_Daily_Summary<- Emb_Wetlands_Raw %>% 
@@ -42,13 +44,8 @@ Abiotic_Daily_Summary$Date<- as.POSIXct(Abiotic_Daily_Summary$Date, format="%y/%
 Abiotic_Daily_Summary$Site<- factor(Abiotic_Daily_Summary$Site, levels=c("Littoral", "Exterior", "Interior"))
 
 
-ggplot(Abiotic_Daily_Summary, aes(Date, DO_mean, group=interaction(Location, Site)))+
-  geom_line()+theme_bw()+theme(axis.text.x =element_text(angle=90))+
-  scale_x_datetime(breaks = ("30 days"))+
-  facet_wrap(~Location+Site)
-
 ggplot(Abiotic_Daily_Summary, aes(Date, DO_mean, group=interaction(Location,Site), color=Site))+
-  geom_line(size=0.8)+geom_hline(yintercept=3, color="#D55E00", lty=2)+geom_hline(yintercept = 0, color="#D55E00")+
+  geom_line(size=0.8)+geom_hline(yintercept=3, color="#D55E00", lty=2)+
   theme_bw()+theme(axis.text.x =element_text(angle=90), legend.position = "bottom", legend.title = element_blank(),
                    strip.text = element_text(size=13),
                    axis.text = element_text(size=13),
@@ -62,4 +59,30 @@ ggplot(Abiotic_Daily_Summary, aes(Date, DO_mean, group=interaction(Location,Site
   facet_wrap(~Location)
 ggsave("Mean_DO_Annual_Trend.png", dpi=300, width=7, height=5)
 
+###################################################################
+############# Grouping by hour ###################################
+####################################################################
 
+Abiotic_Hourly_Summary<- Emb_Wetlands_Raw %>% 
+  mutate(Hour = format(as.POSIXct(Date_Time), format= "%H:%M")) %>% 
+  group_by(across(c(Location, Site, Hour))) %>% 
+  select(DO,TEMP) %>% 
+  summarise(across(c(DO,TEMP), list(mean = mean, min=min, max=max, sd=sd), .names = "{col}_{fn}"))
+
+Abiotic_Hourly_Summary$Site<- factor(Abiotic_Hourly_Summary$Site, levels=c("Littoral", "Exterior", "Interior"))
+Abiotic_Hourly_Summary$Hour<- as.POSIXct(Abiotic_Hourly_Summary$Hour, format= "%H:%M")
+
+ggplot(Abiotic_Hourly_Summary, aes(Hour, DO_mean, group=interaction(Location,Site), color=Site))+
+  geom_line(size=0.8)+geom_hline(yintercept=3, color="#D55E00", lty=2)+
+  theme_bw()+theme(axis.text.x =element_text(angle=90), legend.position = "bottom", legend.title = element_blank(),
+                   strip.text = element_text(size=13),
+                   axis.text = element_text(size=13),
+                   axis.title.x = element_blank(),
+                   axis.title.y = element_text(size=13),
+                   legend.text = element_text(size = 13))+
+  scale_color_manual(values = c("#0072B2", "#009E73", "#999999"))+
+  scale_y_continuous(breaks=seq(0,13,1), limits = c(0,13))+
+  scale_x_datetime(breaks = ("3 hours"), date_labels = "%H:%M")+
+  ylab("Mean Hourly Dissolved Oxygen (mg/l)")+
+  facet_wrap(~Location)
+ggsave("Mean_Hourly_DO.png", dpi = 300, width=7, height = 5)
